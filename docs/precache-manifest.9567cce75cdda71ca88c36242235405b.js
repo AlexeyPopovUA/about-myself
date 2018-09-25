2cd15f069d8b35db520cdf1fc64b42f678d77800
Oleksii Popov <apopov@albelli.net>
2018-09-25 09:14:01 +0200

    Release

---
+++ docs/precache-manifest.9567cce75cdda71ca88c36242235405b.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aeebc5d96842f470456ec485ff2e761",
+    "url": "index.html"
+  },
+  {
+    "url": "index.8ea33cf4a3e14ebbb575.js"
+  },
+  {
+    "url": "index.8ea33cf4a3e14ebbb575.css"
+  }
+];
---
+++ docs/service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.9567cce75cdda71ca88c36242235405b.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/.*.js/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/.*.css/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/.*.html/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/^https:\/\/api.github.com\/gists\/.*/, workbox.strategies.networkFirst(), 'GET');
