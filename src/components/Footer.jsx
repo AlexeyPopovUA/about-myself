e417b83a3466dc3f505773f411574e9cc2f97d31
Oleksii Popov <alexejpopovua@gmail.com>
2018-09-26 21:28:03 +0200

    Footer text should be in the middle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./../../styles/components/Footer.scss";
 export default class Footer {
     static render() {
         return (
-            <footer className="w3-container w3-teal w3-padding footer">
+            <footer className="w3-container w3-teal w3-padding w3-center footer">
                 <div className="author">Developed by O.Popov, 2018</div>
                 <div className="sources">
                     <a href="https://github.com/AlexeyPopovUA/about-myself">GitHub sources</a>
