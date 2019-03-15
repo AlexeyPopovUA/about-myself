1706c7756623fc5316d820c791632c1142db3093
Oleksii Popov <Oleksii.Popov@tomtom.com>
2019-03-15 10:15:21 +0100

    feat(prerendering): implementation candidate of simple prerendering

--- prerender.js
+++ prerender.js
@@ -1,10 +1,13 @@
-//const fs = require('fs');
-//const util = require('util');
+//TODO Transform all that shame into a proper webpack plugin since existing ones do not work
 
-//const readFile = util.promisify(fs.readFile);
+const fs = require('fs');
+const util = require('util');
+const jsdom = require('jsdom');
+const minify = require('html-minifier').minify;
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const writeFile = util.promisify(fs.writeFile);
+
+const {JSDOM} = jsdom;
 const virtualConsole = new jsdom.VirtualConsole();
 
 virtualConsole.on('error', (...args) => {
@@ -20,8 +23,33 @@ virtualConsole.on('dir', (...args) => {
     console.log('dir', args);
 });
 
-JSDOM.fromFile('./dist/index.html',{runScripts: 'dangerously', virtualConsole, resources: "usable"})
-    .then(dom => {
-        setTimeout(() => console.log(dom.serialize()), 2000);
-    })
+const sourceHtml = './dist/index.html';
+const destinationHtml = './dist/index.html';
+const timeout = 2000;
+
+JSDOM.fromFile(sourceHtml, {runScripts: 'dangerously', virtualConsole, resources: 'usable'})
+    //rendering
+    .then(dom => new Promise((resolve, reject) => setTimeout(() => {
+        try {
+            resolve(dom.serialize());
+        } catch (e) {
+            reject(e);
+        }
+    }, timeout)))
+    //minification
+    .then(content => minify(content, {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeTagWhitespace: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true
+    }))
+    //writing result to file
+    .then(content => writeFile(destinationHtml, content, 'utf8'))
     .catch(console.error);
