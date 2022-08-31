b42d871f0ee0fe04bb08cb8e60dc20dc083ee241
Oleksii Popov <alexejpopovua@gmail.com>
2022-08-31 21:53:07 +0200

    fix(additional): hide additional info

--- src/MainView.tsx
+++ src/MainView.tsx
@@ -34,7 +34,7 @@ export default class MainView extends React.Component<Props> {
                         <OwnProjects projects={this.props.renderData.ownProjects} />
                         <Education education={this.props.renderData.education} />
                         {/*<Certifications certificates={this.props.renderData.certificates} />*/}
-                        <AdditionalInfo records={this.props.renderData.additionalInfo} />
+                        {/*<AdditionalInfo records={this.props.renderData.additionalInfo} />*/}
                     </div>
                 </div>
                 <Footer />
