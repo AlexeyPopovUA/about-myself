39c949c9b7f6fece055255bc9336f44062defeb1
Oleksii Popov <alexejpopovua@gmail.com>
2022-10-25 21:34:26 +0200

    fix: minor react-specific issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import data from "./data/data";
+
 import MainView from "./MainView";
 
 if (!DEBUG && 'serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js');
 }
-
-render(<MainView renderData={data}/>, window.document.querySelector(".main"));
+const container = window.document.querySelector(".main");
+const root = createRoot(container);
+root.render(<MainView renderData={data}/>);
 
 // eslint-disable-next-line
 (module as any).hot?.accept();
